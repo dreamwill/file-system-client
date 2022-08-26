db3c65fe401bdc4da286d9d62751b7f8f6e077f2
dreamwill <550992365@qq.com>
2022-08-26 10:08:18 +0800

    Add JavaDoc

--- src/main/java/com/github/dreamwill/fsclient/FileSystemClient.java
+++ src/main/java/com/github/dreamwill/fsclient/FileSystemClient.java
@@ -5,17 +5,69 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public interface FileSystemClient extends Closeable {
+    /**
+     * Connect to the remote file system. This method must be called at first.
+     *
+     * @throws IOException If the remote file system could not be connected.
+     */
     void connect() throws IOException;
 
+    /**
+     * Create a new file and copy bytes from the input stream to it.
+     *
+     * @param path full file path of the new file
+     * @param in   the input stream to copy from
+     * @return <code>true</code> if create and copy successfully; <code>false</code> if this file already exists
+     * @throws IOException If an I/O error occurred
+     */
     boolean createFile(String path, InputStream in) throws IOException;
 
+    /**
+     * Delete the file denoted by this path.
+     *
+     * @param path full file path
+     * @return <code>true</code> if delete successfully; <code>false</code> if the file does not exist
+     * @throws IOException If an I/O error occurred
+     */
     boolean deleteFile(String path) throws IOException;
 
+    /**
+     * Move the file.
+     *
+     * @param from source file path
+     * @param to   target file path
+     * @return <code>true</code> if move successfully; <code>false</code> otherwise
+     * @throws IOException If an I/O error occurred
+     */
     boolean moveFile(String from, String to) throws IOException;
 
+    /**
+     * Copy the file.
+     *
+     * @param from source file path
+     * @param to   target file path
+     * @return <code>true</code> if copy successfully; <code>false</code> otherwise
+     * @throws IOException If an I/O error occurred
+     */
     boolean copyFile(String from, String to) throws IOException;
 
+    /**
+     * Get an InputStream of the file denoted by this path.
+     * <p>
+     * Note: The input stream should be closed after use.
+     *
+     * @param path full file path
+     * @return an input stream
+     * @throws IOException If an I/O error occurred
+     */
     InputStream getInputStream(String path) throws IOException;
 
+    /**
+     * Get metadata of the file denoted by this path.
+     *
+     * @param path full file path
+     * @return metadata
+     * @throws IOException If an I/O error occurred
+     */
     FileMetadata getFileMetadata(String path) throws IOException;
 }
